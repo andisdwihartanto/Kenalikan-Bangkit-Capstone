5a225f1a903c67dbf6d15e4273953b6e8e3d4caf
adriannsyahn <akunbelajjar01@gmail.com>
2024-06-15 23:00:06 +0700

    add top menu

--- MD/Kenalikan/app/src/main/java/com/dicoding/kenalikan/MainActivity.kt
+++ MD/Kenalikan/app/src/main/java/com/dicoding/kenalikan/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.dicoding.kenalikan.databinding.ActivityMainBinding
 import com.dicoding.kenalikan.fragment.CameraFragment
 import com.dicoding.kenalikan.fragment.HomeFragment
 import com.dicoding.kenalikan.fragment.RentFragment
@@ -13,6 +14,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var binding: ActivityMainBinding
 
     private val viewModel by viewModels<MainViewModel> {
         ViewModelFactory.getInstance(this)
@@ -20,7 +22,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         viewModel.getSession().observe(this) { user ->
             if (!user.isLogin) {
@@ -29,28 +32,48 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        bottomNavigationView = findViewById(R.id.bottomNav)
-        bottomNavigationView.setOnItemSelectedListener { menuItem ->
-            when(menuItem.itemId) {
-                R.id.idBeranda -> {
-                    replaceFragment(HomeFragment())
+            binding.topAppBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.idProfile -> {
+
                     true
                 }
-                R.id.idKamera -> {
-                    replaceFragment(CameraFragment())
-                    true
-                }
-                R.id.idRental -> {
-                    replaceFragment(RentFragment())
+                R.id.idKeluar -> {
+                    val intent = Intent(this, WelcomeActivity::class.java)
+                    startActivity(intent)
                     true
                 }
                 else -> false
             }
         }
-        replaceFragment(HomeFragment())
-    }
 
-    private fun replaceFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit()
-    }
+            bottomNavigationView = findViewById(R.id.bottomNav)
+            bottomNavigationView.setOnItemSelectedListener { menuItem ->
+                when (menuItem.itemId) {
+                    R.id.idBeranda -> {
+                        replaceFragment(HomeFragment())
+                        true
+                    }
+
+                    R.id.idKamera -> {
+                        replaceFragment(CameraFragment())
+                        true
+                    }
+
+                    R.id.idRental -> {
+                        replaceFragment(RentFragment())
+                        true
+                    }
+
+                    else -> false
+                }
+            }
+            replaceFragment(HomeFragment())
+        }
+
+
+
+        private fun replaceFragment(fragment: Fragment) {
+            supportFragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit()
+        }
 }
